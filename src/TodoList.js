8ded97de1ab8fe0d51b16cedde69140afb80a20d
diegotesch <diego.tesch@gmail.com>
2020-10-05 22:56:54 -0300

    adicionada action remover todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,17 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as TodoActions from "./store/actions/todos";
 
-const TodoList = ({ todos, addTodo }) => (
+const TodoList = ({ todos, addTodo, removeTodo }) => (
   <Fragment>
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => removeTodo(todo.id)}>Remover</button>
+        </li>
       ))}
     </ul>
     <button onClick={() => addTodo("Programar")}>Adicionar</button>
@@ -15,6 +20,7 @@ const TodoList = ({ todos, addTodo }) => (
 
 TodoList.propTypes = {
   addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
@@ -29,8 +35,7 @@ const mapStateToProps = (state) => ({
 });
 
 // transforma actions em propriedades do componente
-const mapDispatchToProps = (dispatch) => ({
-  addTodo: (text) => dispatch({ type: "ADD_TODO", payload: { text } }),
-});
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(TodoActions, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
